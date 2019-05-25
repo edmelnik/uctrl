f99a96789e93497668a55a6a0a0d5a9c4a037fef
Aditya Naik <naik.aditya23@gmail.com>
2019-05-25 13:17:02 -0400

    switched adc to just headers

--- src/adc.c
+++ src/adc.c
@@ -9,56 +9,7 @@ Should ideally use functions in serial.c for output
 /* #include <string.h> */
 
 /* #include "serial.h" */
-#include "adc.h"
-// next two are included in serial
 
 
-void adcInit(){
-    ADMUX = (1<<REFS0);
-    ADCSRA = (1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);
-}
 
-uint16_t readADC(uint8_t ch){
-    ch &= 0b00000111;  // AND operation with 7
-    ADMUX = (ADMUX & 0xF8)|ch;     // clears the bottom 3 bits before ORing
-    // start single conversion
-    // write '1' to ADSC
-    ADCSRA |= (1<<ADSC);
- 
-    // wait for conversion to complete
-    // ADSC becomes '0' again
-    // till then, run loop continuously
-    while(ADCSRA & (1<<ADSC)); 
-    return (ADC);
-}
 
-// Setup serial output streams for stdio
-/* FILE uart_output = FDEV_SETUP_STREAM(uartPutchar, NULL, */
-/* 				     _FDEV_SETUP_WRITE); */
-/* FILE uart_input = FDEV_SETUP_STREAM(NULL, uartGetchar, */
-/* 				    _FDEV_SETUP_READ); */
-/* FILE uart_io = FDEV_SETUP_STREAM(uartPutchar, uartGetchar, */
-/* 				 _FDEV_SETUP_RW); */
-
-/* int main(){ */
-/*     initADC(); */
-/*     uartInit(); */
-    
-/*     stdout = &uart_output; */
-/*     stdin = &uart_input; */
-    
-/*     uint16_t adc_read_val; */
-/*     char msg[20]; */
-/*     DDRB |= _BV(DDB5); */
-/*     while(1){ */
-/* 	adc_read_val = readADC(0); */
-/* 	PORTB |= _BV(PORTB5); */
-/* 	// For some reason the value is twice actual input voltage */
-/* 	// Might be a good idea to consult the datasheets at some point */
-/* 	//  to figure out why */
-/* 	printf("%d\n", adc_read_val/2);  */
-/* 	_delay_ms(PRINT_DELAY_MS); */
-/* 	PORTB |= _BV(PORTB5); */
-/*     } */
-
-/* } */
--- src/adc.h
+++ src/adc.h
@@ -1,4 +1,3 @@
-
 #include <stdint.h>
 #include <stdlib.h>
 
@@ -7,5 +6,21 @@
 /* #include <util/setbaud.h> */
 #include <avr/io.h>
 
-void adcInit();
-uint16_t readADC(uint8_t ch);
+void adcInit(){
+    ADMUX = (1<<REFS0);
+    ADCSRA = (1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);
+}
+uint16_t readADC(uint8_t ch){
+    ch &= 0b00000111;  // AND operation with 7
+    ADMUX = (ADMUX & 0xF8)|ch;     // clears the bottom 3 bits before ORing
+    // start single conversion
+    // write '1' to ADSC
+    ADCSRA |= (1<<ADSC);
+ 
+    // wait for conversion to complete
+    // ADSC becomes '0' again
+    // till then, run loop continuously
+    while(ADCSRA & (1<<ADSC)); 
+    return (ADC);
+}
+
