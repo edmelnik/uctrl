4752cfffdbf001923998561b72e65c1931776317
Aditya Naik <naik.aditya23@gmail.com>
2019-05-28 11:21:10 -0400

    better errors

--- modified_read_sensor.ino
+++ modified_read_sensor.ino
@@ -29,19 +29,18 @@
 uint32_t prev = 0;
 const uint32_t interval = 10;
 
-struct pressure_values{
-    int p_val[4];
-};
-
-struct pressure_values p_struct;
-
 void setup(){
     delay(200);            // my particular board needs this
     Serial.begin(9600);
     Wire.begin();
 }
 
-int get_i2c(int mux_addr, int mux_channel, int device_addr, int min, int max, char* pressure_out){
+int get_i2c(int mux_addr, int mux_channel, int device_addr,
+	    int min, int max, char* pressure_out){
+
+    char errstr[20] = "ERR", *errval;
+    errval = malloc(3);
+    
     Wire.beginTransmission(mux_addr);
     Wire.write(1 << mux_channel);
     Wire.endTransmission();
@@ -53,60 +52,17 @@ int get_i2c(int mux_addr, int mux_channel, int device_addr, int min, int max, ch
     float p, t;
     el = ps_get_raw(device_addr, &ps);
     if(el <= 4 && el >= 1){
-	/* Serial.println("FAILED"); */
-	strcpy(pressure_out, "ERR");
+	strcat(errstr, itoa(el, errval, 10));
+	strcpy(pressure_out, errstr);
 	return -1;
     }
     else{
 	ps_convert(ps, &p, &t, OUTPUT_MIN, OUTPUT_MAX, min, max);
 	dtostrf(p, 2, 3, p_str); 
-	dtostrf(t, 2, 3, t_str);
-	
-	/* dtostrf(ps.bridge_data, 2, 2, p_raw_str); */
-	/* Serial.println(p_raw_str); */
-	/* Serial.println(p_str); */
-	
+	dtostrf(t, 2, 3, t_str);	
 	strcpy(pressure_out, p_str);
-	/* p_struct.p_val[mux_channel-1] = p; // indexed by 0 */
 	return 1;
-    }
-        
-    
-    /* if ((now - prev > interval) && (Serial.available() <= 0)){
-    /* 	prev = now; */
-    /* 	el = ps_get_raw(device_addr, &ps); */
-    /* 	// for some reason my chip triggers a diagnostic fault */
-    /* 	// on 50% of powerups without a notable impact */
-    /* 	// to the output values. */
-    /* 	if ( el == 4 ) { */
-    /* 	    Serial.println("err sensor missing"); */
-    /* 	} */
-    /* 	else { */
-    /* 	    if ( el == 3 ) { */
-    /* 		Serial.print("err diagnostic fault "); */
-    /* 		Serial.println(ps.status, BIN); */
-    /* 	    } */
-    /* 	    if ( el == 2 ) { */
-    /* 		// if data has already been feched since the last */
-    /* 		// measurement cycle */
-    /* 		Serial.print("warn stale data "); */
-    /* 		Serial.println(ps.status, BIN); */
-    /* 	    } */
-    /* 	    if ( el == 1 ) { */
-    /* 		// chip in command mode */
-    /* 		// no clue how to end up here */
-    /* 		Serial.print("warn command mode "); */
-    /* 		Serial.println(ps.status, BIN); */
-    /* 	    } */
-    /* 	    ps_convert(ps, &p, &t, OUTPUT_MIN, OUTPUT_MAX, min, max); */
-    /* 	    // floats cannot be easily printed out */
-    /* 	    dtostrf(p, 2, 2, p_str); */
-    /* 	    dtostrf(t, 2, 2, t_str); */
-	    
-    /* 	    Serial.println(p); */
-    /* 	    return; */
-    /* 	} */
-    /* } */
+    }           
 }
 
 void loop(){
@@ -115,39 +71,15 @@ void loop(){
     int i, retval, buf_ptr = 0;
 
     buffer = malloc(50);
+
     pressure = malloc(10);
     // indexed by 1 to correspond with mux_channel
     for(i=1; i<5; i++){
 	retval = get_i2c(MUXADDR, i, SLAVE_ADDR,
 			 P_MIN0, P_MAX0, pressure);
-	/* Serial.println(pressure); */
-	if(retval<0){ // Error
-	    /* Serial.print(pressure); */
-	    errs[i-1] = retval; // store the error here for now
-	}
-	else{
-	    /* Serial.print(pressure); */
-	    // nothing needs to be done, since 
-	    // pressure values are in the global struct p_struct
-	    /* strcpy(buffer+buf_ptr, *pressure); */
-	    /* buf_ptr+=strlen(pressure); */
-	    /* Serial.print(pressure); */
-	}
 	buf_ptr += snprintf(buffer+buf_ptr, 50-buf_ptr,
 			    " %s ", pressure);
     }
-    /* snprintf(buffer+buf_ptr, 50-buf_ptr, "\n"); */
     Serial.println(buffer);
-    /* for(i=0; i<4; i++){ */
-    /* 	if(errs[i] == 1) */
-    /* 	    buf_ptr += snprintf(buffer, 50-buf_ptr, " %s ", err_str); */
-    /* 	else */
-    /* 	    buf_ptr += snprintf(buffer, 50-buf_ptr, " %d ", */
-    /* 				p_struct.p_val[i]); */
-    /* } */
-    
-    Serial.println(buffer);
-    /* get_i2c(MUXADDR, SLAVE0_MUX, SLAVE_ADDR, P_MIN0, P_MAX0); */
-    /* get_i2c(MUXADDR, SLAVE1_MUX, SLAVE1_ADDR, P_MIN1, P_MAX1); */
     delay(600);
 }
