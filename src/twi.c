eacdb1caef5b9988803a6abf43beeff6cb31273b
Aditya Naik <naik.aditya23@gmail.com>
2019-05-29 17:45:40 -0400

    new twi

---
+++ src/twi.c
@@ -0,0 +1,12 @@
+#include <avr/io.h>
+#include <util/twi.h>
+#include "serial.h"
+
+int main(){
+    /*
+      Required operations
+      - write sensor ID (mux channel) to mux address (0x70)
+      - write request for data to sensor address (0x28)
+      - 
+     */
+}
