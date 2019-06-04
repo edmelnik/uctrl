e781583bf07577da37d9a2dcbbfd974fc8afffc7
Aditya Naik <naik.aditya23@gmail.com>
2019-06-04 13:29:23 -0400

    reader work; added config stuff

--- src/pressure/reader.py
+++ src/pressure/reader.py
@@ -23,27 +23,37 @@ ERR4: Sensor missing (check the PCB if sensor is expected to be in place)
 
 '''
 TODO Add logging of errors and other messages (TIMEOUT, connecting to device on address)
-TODO A better way of reading what to do with the collected datapoints might be to have a config file in the same directory as this script: the config can allow various operations
-  - print data to STDOUT
-  - push data to zigbee
-  - put data into SQL/Influx
-  - put data into a csv file
-  The output data format could also be specified in the config
-  The source for each of these actions should be outside this file and imported by this file using ConfigParser (catch errors if import fails)
 TODO use logic level converter to output on serial pins as well
 TODO Add reset condition if 3 or more timeouts detected
   - Number of timeouts to reset could be a config option
 TODO Find the reason for periodic timeouts - for some reason communication seems to stop and the port needs to be restarted in order for it to work
  - This seems to be related with the number of devices the microcontroller is serving
 TODO Inspect why error numbers ERR* seem to periodically disappear (itoa problem?)
+DONE A better way of reading what to do with the collected datapoints might be to have a config file in the same directory as this script: the config can allow various operations
+  - print data to STDOUT
+  - push data to zigbee
+  - put data into SQL/Influx
+  - put data into a csv file
+  The output data format could also be specified in the config
+  The source for each of these actions should be outside this file and imported by this file using ConfigParser (catch errors if import fails)
 DONE Add timestamps to data
 DONE if ACM0 does no exist, catch that exception and try other ttyACM*
 DONE ignore all recieved data for the first 2 seconds
 '''
 
+'''
+NOTES and thoughts
+- error logging should be smart: every error occurence should not be necessarily logged since in the main while loop the error would occur many times a second. can use a timer to time error occurence frequency (is there a module for this?)
+'''
+
 import serial
 import time
 import sys
+import configparser
+
+# Output values
+STDOUT = 'stdout'
+XBEE = 'zigbee'
 
 def connect():
     connected = False
@@ -95,17 +105,34 @@ def printData(values):
         print(output)
     except IndexError:
         pass
+    
+'''
+This function returns only the output section from config file
+in the future, if more sections are needed, perhaps a custom config
+class would be easier (or maybe a function with a section-specifying parameter)
+'''
+def readOutputConfig(config):
+    config.read("config")
+    return config['output']
+
+def doOutput(output, values):
+    if int(output[XBEE]) == 1:
+        pass
+    if int(output[STDOUT]) == 1: # not elif for more than one output
+        printData(values)    
         
 def main():
     device = connect()
+    config = configparser.ConfigParser()
     initData(device)
     while True:
+        config.read("config")
+        output = readOutputConfig(config)
         values = getData(device)
         if len(values) <= 1: # only got the currtime
             device = handleTimeout(device)
         else:
-            # do something with data (printing for now)
-            printData(values)
+            doOutput(output, values)
 
 # Don't stop even if device gets disconnected            
 while True:
@@ -116,5 +143,5 @@ while True:
         sys.exit()
     except serial.SerialException:
         continue
-    except:
-        continue
+    # except:
+    #     continue
