aad190196dc582326df3eb1e523536f13cd0029c
Aditya Naik <naik.aditya23@gmail.com>
2019-06-06 20:16:54 -0400

    errors

--- src/o2/o2.ino
+++ src/o2/o2.ino
@@ -27,9 +27,10 @@ Will the ~ PWM pins with this? Should any strange and unexpected errors occur in
 #define STATUS_REG 30004
 #define ERR_REG    30005
 // Holding regs
-#define ONOFF_REG  40001
-#define CLCTRL_REG 40004 // Calibration control
-#define ADDR_REG   40006
+#define ONOFF_REG   40001
+#define CLCTRL_REG  40004 // Calibration control
+#define ADDR_REG    40006
+#define ERR_CLR_REG 40002
 
 // Starting PIN on the microcontroller
 const static int START_PIN PROGMEM = 4;
@@ -41,13 +42,14 @@ const static int ON       PROGMEM = 2;
 const static int SHUTDOWN PROGMEM = 3;
 const static int STANDBY  PROGMEM = 4;
 
-SoftwareSerial modbus[NUM_SENSORS] = {
-    SoftwareSerial(START_PIN, START_PIN+1),
-    SoftwareSerial(START_PIN+2, START_PIN+3),
-    SoftwareSerial(START_PIN+4, START_PIN+5),
-    SoftwareSerial(START_PIN+6, START_PIN+7),
-};
+// SoftwareSerial modbus[NUM_SENSORS] = {
+//     SoftwareSerial(START_PIN, START_PIN+1),
+//     SoftwareSerial(START_PIN+2, START_PIN+3),
+//     SoftwareSerial(START_PIN+4, START_PIN+5),
+//     SoftwareSerial(START_PIN+6, START_PIN+7),
+// };
 
+SoftwareSerial modbus(6, 7);
 // SoftwareSerial *modbus = malloc(sizeof(SoftwareSerial)*4);
 
 // ModbusMaster node[NUM_SENSORS];
@@ -63,44 +65,54 @@ int status[NUM_SENSORS], response[NUM_SENSORS], err[NUM_SENSORS];
 DONE parse protocol errors
 
 Response codes
+
 0  Success
--1 invalid response slave ID exception
--2 invalid response function exception
--3 response timed out exception
--4 invalid response CRC exception
+1 invalid response slave ID exception
+2 invalid response function exception
+3 response timed out exception
+4 invalid response CRC exception
+5 modbus proto illegal func
+6 modbus proto illegal data address
+7 modbus proto illegal data value
+8 modbus proto device failure
 
 4-20ma.io/ModbusMaster/group__constant.html#gaf69c1c360b84adc5bf1c7bbe071f1970
 
 This func expects int representations of hex error values and converts them 
-into errors in [-1, -4]
+into errors in [-1, -8]
 */
-int parseError(float e){
+int parseError(int e){
     int ret;
     if(e == 0)
 	ret = e;
+    else if(e >= 224 && e <= 227)
+	ret = 0-(e%223);
+    else if(ret >= 1 && ret <= 4)
+	ret = 0-(e+4);
     else
-	ret = 0-((int)e % 223);
-    return ret;
+	ret = -7; // unknown error
+    return ret; 
 }
 
-float readReg(int sensor, int reg){
+int readReg(int sensor, int reg){
     int res;
-    float val;
-    modbus[sensor].begin(BAUD);
+    int val;
+    // modbus[sensor].begin(BAUD);
     res = node[sensor].readInputRegisters(reg, 1);
     if(res == 0){
 	val = node[sensor].getResponseBuffer(0);
 	node[sensor].clearResponseBuffer();
     }
-    else
+    else{
 	return parseError(res);
+    }
     return val;    
 }
 
 // If this func is taking an int parameter for value, can it be used for calibration?
 int writeReg(int sensor, int reg, int value){
     int res;
-    modbus[sensor].begin(BAUD);
+    // modbus[sensor].begin(BAUD);
     res = node[sensor].writeSingleRegister(reg, value);
     return parseError(res);
 }
@@ -108,28 +120,28 @@ int writeReg(int sensor, int reg, int value){
 void setup(){    
     int i, curr_pin, SST_addr;    
     Serial.begin(BAUD); // To USB output
-   
+    modbus.begin(BAUD);
     // Actual sensor addresses are indexed by 1, but
     // once sensor addresses are configured in node[], they should indexed by 0
     for(i=0; i<NUM_SENSORS; i++){
     	SST_addr = i+1;
-    	modbus[i].begin(BAUD);
+    	// modbus[i].begin(BAUD);
 	node[i] = *(new ModbusMaster);
-    	node[i].begin(SST_addr, modbus[i]);
+    	// node[i].begin(SST_addr, modbus[i]);
+	node[i].begin(SST_addr, modbus);
     }
     
     // Get status
     for(i=0; i<NUM_SENSORS; i++){
-	modbus[i].begin(BAUD);
 	status[i] = readReg(i, STATUS_REG);
-	err[i] = status[i];
+	// err[i] = status[i];
     }
     
     // If sensor is idle, turn it on
     for(i=0; i<NUM_SENSORS; i++){
-	modbus[i].begin(BAUD);
 	if(status[i] == IDLE || status[i] == STANDBY)
-	    err[i] = writeReg(i, ONOFF_REG, 1);
+	    status[i] = writeReg(i, ONOFF_REG, 1);
+	    // err[i] = writeReg(i, ONOFF_REG, 1);
     }
 
     // At this point ideally all sensors are turned on.
@@ -146,37 +158,56 @@ void setup(){
 // TODO This should ideally be based on a timer instead of looops
 
 int k = 0;
-
-Void handleSensor(int i){
+int reset[] = {0,0,0,0};// 1: reset
+void handleSensor(int i){    
     status[i] = readReg(i, STATUS_REG);
-    err[i] = status[i];
     if(status[i] == IDLE || status[i] == STANDBY)
-	err[i] = writeReg(i, ONOFF_REG, 1);
+	status[i] = writeReg(i, ONOFF_REG, 1);
+    else if(status[i]<0 && reset[i] == 0){
+    	// clear warnings
+    	status[i] = writeReg(i, ERR_CLR_REG, 1);
+    	reset[i] = 1;
+    }
+    // else if(status[i] <0 && reset[i] == 1){
+    // 	// restart sensor
+    // 	status[i] = writeReg(i, ONOFF_REG, 0);
+    // 	reset[i] = 0;
+    // }
 }
 
 void loop(){
     int i, buf_ptr = 0;
-    float data;    
+    int data;    
     char *buffer;
     buffer = malloc(50);
     
     for(i=0; i<NUM_SENSORS; i++){
 	char errstr[20] = "ERR", *errval, output[10];
+	char statstr[10] = "STS";
+	
 	errval = malloc(3);
-	modbus[i].begin(BAUD);
-	if(status[i] == ON){
+	if((status[i] == ON || status[i] == STANDBY)){
 	    data = readReg(i, O2AVG_REG);
-	    if(data < 0){ // Error
-		// do something
+	    if(data <= 0){ // Error
+		status[i] = data;
+		if(k==10)
+		    handleSensor(i);
 	    }
-	    else
-		dtostrf(data, 2, 3, output);
+	    else{
+		dtostrf(data, 4, 0, output);
+	    }
 	}
-	else{	    
-	    strcat(errstr, itoa(-1*err[i], errval, 10));
+	else if(status[i] < 0){ // Error status
+	    strcat(errstr, itoa(status[i]*-1, errval, 10));
 	    strcpy(output, errstr);
 	    // Get status
-	    if(k==100)
+	    if(k==10)
+		handleSensor(i);
+	}
+	else{ // No error but sensor is not idling or standby
+	    strcat(statstr, itoa(status[i], errval, 10));
+	    strcpy(output, statstr);
+	    if(k==10)
 		handleSensor(i);
 	}
 	buf_ptr += snprintf(buffer+buf_ptr, 50-buf_ptr,
@@ -188,8 +219,8 @@ void loop(){
     free(buffer);
     
     k+=1;
-    k%=101;    
-    // delay(100);
+    k%=11;    
+    delay(100);
 }
 
 
