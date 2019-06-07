84ab7f2e462a119ff11b86987cef084db2dbaaa4
Aditya Naik <naik.aditya23@gmail.com>
2019-06-06 22:22:15 -0400

    working 3 sensors, might have to change the delay on line 190 to get 4 to work

--- src/o2/o2.ino
+++ src/o2/o2.ino
@@ -1,17 +1,17 @@
 /*
 
-TODO Better error checking and overall control flow
+  TODO Better error checking and overall control flow
   - Periodically monitor status and check expected O2 values
   - O2 values could be modelled to make intelligent calibration decisions
-TODO Format the output values in a similar manner to how the pressure is being formatted
+  TODO Format the output values in a similar manner to how the pressure is being formatted
   - Might be possible to use the output function as a header file common to both
-TODO calibration routine
-TODO When to use STANDBY mode on sensors?
-TODO Read multiple registers and parse the response buffer
+  TODO calibration routine
+  TODO When to use STANDBY mode on sensors?
+  TODO Read multiple registers and parse the response buffer
   - Select relevent values (see comment above func loop)
-TODO Combine o2 and pressure into a single grand PoC
+  TODO Combine o2 and pressure into a single grand PoC
 
-Will the ~ PWM pins with this? Should any strange and unexpected errors occur in the near future, blame this
+  Will the ~ PWM pins with this? Should any strange and unexpected errors occur in the near future, blame this
 */
 
 #include <ModbusMaster.h>
@@ -49,7 +49,7 @@ const static int STANDBY  PROGMEM = 4;
 //     SoftwareSerial(START_PIN+6, START_PIN+7),
 // };
 
-SoftwareSerial modbus(6, 7);
+SoftwareSerial modbus(4, 5);
 // SoftwareSerial *modbus = malloc(sizeof(SoftwareSerial)*4);
 
 // ModbusMaster node[NUM_SENSORS];
@@ -62,42 +62,37 @@ int status[NUM_SENSORS], response[NUM_SENSORS], err[NUM_SENSORS];
 
 /*
 
-DONE parse protocol errors
+  DONE parse protocol errors
 
-Response codes
+  Response codes
 
-0  Success
-1 invalid response slave ID exception
-2 invalid response function exception
-3 response timed out exception
-4 invalid response CRC exception
-5 modbus proto illegal func
-6 modbus proto illegal data address
-7 modbus proto illegal data value
-8 modbus proto device failure
+  0  Success
+  1 invalid response slave ID exception
+  2 invalid response function exception
+  3 response timed out exception
+  4 invalid response CRC exception
+  5 modbus proto illegal func
+  6 modbus proto illegal data address
+  7 modbus proto illegal data value
+  8 modbus proto device failure
 
-4-20ma.io/ModbusMaster/group__constant.html#gaf69c1c360b84adc5bf1c7bbe071f1970
+  4-20ma.io/ModbusMaster/group__constant.html#gaf69c1c360b84adc5bf1c7bbe071f1970
 
-This func expects int representations of hex error values and converts them 
-into errors in [-1, -8]
+  This func expects int representations of hex error values and converts them 
+  into errors in [-1, -8]
 */
 int parseError(int e){
     int ret;
-    if(e == 0)
-	ret = e;
-    else if(e >= 224 && e <= 227)
-	ret = 0-(e%223);
-    else if(ret >= 1 && ret <= 4)
+    if(e >= 224 && e <= 227)
+	ret = 0-(e % 223);
+    else if(e >=1 && e <= 4)
 	ret = 0-(e+4);
-    else
-	ret = -7; // unknown error
-    return ret; 
+    return ret;
 }
 
 int readReg(int sensor, int reg){
     int res;
     int val;
-    // modbus[sensor].begin(BAUD);
     res = node[sensor].readInputRegisters(reg, 1);
     if(res == 0){
 	val = node[sensor].getResponseBuffer(0);
@@ -106,15 +101,30 @@ int readReg(int sensor, int reg){
     else{
 	return parseError(res);
     }
-    return val;    
+    return val;
 }
 
 // If this func is taking an int parameter for value, can it be used for calibration?
 int writeReg(int sensor, int reg, int value){
-    int res;
-    // modbus[sensor].begin(BAUD);
+    int res, ret;
     res = node[sensor].writeSingleRegister(reg, value);
-    return parseError(res);
+    if(res == 0)
+	ret = res;
+    else
+	ret = parseError(res);
+    return ret;
+}
+
+void handleSensor(int i){
+    int res;
+    status[i] = readReg(i, STATUS_REG);
+    if(status[i] == IDLE || status[i] == STANDBY){
+	res = writeReg(i, ONOFF_REG, 1);
+	if(res < 0)
+	    status[i] = res;
+	else
+	    status[i] = readReg(i, STATUS_REG);
+    }
 }
 
 void setup(){    
@@ -125,23 +135,14 @@ void setup(){
     // once sensor addresses are configured in node[], they should indexed by 0
     for(i=0; i<NUM_SENSORS; i++){
     	SST_addr = i+1;
-    	// modbus[i].begin(BAUD);
 	node[i] = *(new ModbusMaster);
-    	// node[i].begin(SST_addr, modbus[i]);
-	node[i].begin(SST_addr, modbus);
-    }
+	node[i].begin(SST_addr, modbus);	
     
-    // Get status
-    for(i=0; i<NUM_SENSORS; i++){
-	status[i] = readReg(i, STATUS_REG);
-	// err[i] = status[i];
-    }
-    
+    }   
     // If sensor is idle, turn it on
     for(i=0; i<NUM_SENSORS; i++){
 	if(status[i] == IDLE || status[i] == STANDBY)
 	    status[i] = writeReg(i, ONOFF_REG, 1);
-	    // err[i] = writeReg(i, ONOFF_REG, 1);
     }
 
     // At this point ideally all sensors are turned on.
@@ -149,78 +150,53 @@ void setup(){
     //  should be handled appropriately in loop()
 }
 
-// TODO Output format
-// - STATUS CAL O2%avg O2%raw ERR Heater_voltage ppO2real Pressure PSTemp
-// - 30004 30018 30001 30002  30005 30006        30014    30016    30017
-// - outputting just o2avg for now
-
-// Every 10 loops, check for status to make sure sensor is on and no errors being sent
-// TODO This should ideally be based on a timer instead of looops
-
-int k = 0;
-int reset[] = {0,0,0,0};// 1: reset
-void handleSensor(int i){    
-    status[i] = readReg(i, STATUS_REG);
-    if(status[i] == IDLE || status[i] == STANDBY)
-	status[i] = writeReg(i, ONOFF_REG, 1);
-    else if(status[i]<0 && reset[i] == 0){
-    	// clear warnings
-    	status[i] = writeReg(i, ERR_CLR_REG, 1);
-    	reset[i] = 1;
-    }
-    // else if(status[i] <0 && reset[i] == 1){
-    // 	// restart sensor
-    // 	status[i] = writeReg(i, ONOFF_REG, 0);
-    // 	reset[i] = 0;
-    // }
-}
+int k=0;
 
 void loop(){
-    int i, buf_ptr = 0;
-    int data;    
+    int i, buf_ptr = 0, data;    
     char *buffer;
+    
     buffer = malloc(50);
     
     for(i=0; i<NUM_SENSORS; i++){
-	char errstr[20] = "ERR", *errval, output[10];
+	char errstr[10] = "ERR", *errval, output[10];
 	char statstr[10] = "STS";
-	
 	errval = malloc(3);
-	if((status[i] == ON || status[i] == STANDBY)){
+
+	if(status[i] == ON){
 	    data = readReg(i, O2AVG_REG);
-	    if(data <= 0){ // Error
-		status[i] = data;
-		if(k==10)
-		    handleSensor(i);
+	    if(data < 0){ // Error
+		handleSensor(i);
 	    }
 	    else{
 		dtostrf(data, 4, 0, output);
 	    }
 	}
-	else if(status[i] < 0){ // Error status
-	    strcat(errstr, itoa(status[i]*-1, errval, 10));
-	    strcpy(output, errstr);
-	    // Get status
-	    if(k==10)
-		handleSensor(i);
-	}
-	else{ // No error but sensor is not idling or standby
+	else if(status[i] > 0){
 	    strcat(statstr, itoa(status[i], errval, 10));
 	    strcpy(output, statstr);
-	    if(k==10)
+	    if(k==100)
+		handleSensor(i);
+	}
+	else{	    
+	    strcat(errstr, itoa(status[i]*-1, errval, 10));
+	    strcpy(output, errstr);
+	    if(k==100)
 		handleSensor(i);
 	}
 	buf_ptr += snprintf(buffer+buf_ptr, 50-buf_ptr,
 			    " %s ", output);
 	free(errval);
+	delay(3);
     }
+    
     Serial.println(buffer);
     Serial.flush();
     free(buffer);
     
     k+=1;
-    k%=11;    
-    delay(100);
+    k%=101;    
+    // delay(100);
 }
 
 
